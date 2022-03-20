5a82fd4c5b22a4989782b811ba6e93e9a1cdd186
daruomo <yumebito.0614@gmail.com>
2022-03-21 04:58:43 +0900

    デモ用に修正

--- flutter_application/lib/res/custom_text.dart
+++ flutter_application/lib/res/custom_text.dart
@@ -1,8 +1,7 @@
 class CustomText {
   static final Map genreOfDestination = {
-    0: {'title': '観光地', 'state': false, 'path': 'tourist_spot.png'},
-    1: {'title': 'フォトスポット', 'state': false, 'path': 'photo_spot.png'},
-    2: {'title': '地元グルメ', 'state': false, 'path': 'gourmand.png'},
-    3: {'title': 'カフェ', 'state': false, 'path': 'cafe.png'},
+    0: {'title': '観光', 'state': false, 'path': 'tourist_spot.png'},
+    1: {'title': '飲食店', 'state': false, 'path': 'gourmand.png'},
+    2: {'title': '景観', 'state': false, 'path': 'photo_spot.png'},
   };
 }
